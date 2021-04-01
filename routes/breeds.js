4d40801b510d1472f91f129b0d3701304696e58b
Nicholas Lamb <nickplamb@gmail.com>
2021-03-31 21:05:50 -0400

    add comments and clean up breeds routes

--- routes/breeds.js
+++ routes/breeds.js
@@ -5,33 +5,22 @@ const Models = require('../models/Breed.js');
 
 const Breed = Models.Breed;
 
-// app.get('/breeds', breedRoutes.breeds);
-// app.get('/breeds/:breed', breedRoutes.breedByName);
-// app.get('/breeds/eggs/:color', breedRoutes.eggColor);
-// app.get('/breeds/class/:class', breedRoutes.apaClass);
-
-// Handler for GET /breeds
-/*
- * @params request, response
- */
+// Get all chicken breeds
 router.get('/', (req, res) => {
   Breed.find()
     .then((breeds) => {
-      return res.json(breeds);
+      return res.status(200).json(breeds);
     })
     .catch((err) => {
       return res.status(500).send(`Something went wrong. Error: ${err}.`);
     });
 });
 
-// Handler for GET /breeds/:breed
-/*
- * CASE SENSITIVE
- * @params req.params.breed
- */
+// Get one breed by name
 router.get('/:breed', (req, res) => {
   Breed.findOne({ breed: req.params.breed })
     .then((breed) => {
+      // No breed found. Abort
       if (!breed) {
         return res.status(404).send('There are no breed in the database by that name.');
       }
@@ -42,13 +31,11 @@ router.get('/:breed', (req, res) => {
     });
 });
 
-// Handler for GET /breeds/eggs/:color
-/*
- * @params req.params.color
- */
+// Get all breeds by egg color
 router.get('/eggs/:color', (req, res) => {
   Breed.find({ eggColor: req.params.color.toLowerCase() })
     .then((breeds) => {
+      // No Breeds Found. Abort
       if (breeds.length === 0) {
         return res
           .status(404)
@@ -61,13 +48,11 @@ router.get('/eggs/:color', (req, res) => {
     });
 });
 
-// Handler for GET /breeds/class/:class
-/*
- * @params req.params.class
- */
+// Get all breeds of specified class
 router.get('/class/:class', (req, res) => {
   Breed.find({ apaClass: req.params.class })
     .then((breeds) => {
+      // No breeds found. Abort.
       if (breeds.length === 0) {
         return res
           .status(404)
