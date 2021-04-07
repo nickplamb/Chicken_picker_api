50ca3f128a0d1da77d78166d3ec872b80f3c2892
Nicholas Lamb <nickplamb@gmail.com>
2021-04-07 00:30:12 -0400

    create breedController and move controller functions into it from routes file.

---
+++ controllers/breedController.js
@@ -0,0 +1,69 @@
+// Models
+const { Breed } = require('../models/Breed.js');
+
+// DB connection
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/chickendb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Get all chicken breeds
+exports.breed_get_all_breeds = function (req, res) {
+  Breed.find()
+    .then((breeds) => {
+      return res.status(200).json(breeds);
+    })
+    .catch((err) => {
+      return res.status(500).send(`Something went wrong. Error: ${err}.`);
+    });
+};
+
+// Get one breed by name
+exports.breeds_get_breed_by_name = function (req, res) {
+  Breed.findOne({ breed: req.params.breed })
+    .then((breed) => {
+      // No breed found. Abort
+      if (!breed) {
+        return res.status(404).send('There are no breed in the database by that name.');
+      }
+      return res.status(200).json(breed);
+    })
+    .catch((err) => {
+      return res.status(500).send(`Error: ${err}`);
+    });
+};
+
+// Get all breeds by egg color
+exports.breed_get_breeds_by_egg_color = function (req, res) {
+  Breed.find({ eggColor: req.params.color.toLowerCase() })
+    .then((breeds) => {
+      // No Breeds Found. Abort
+      if (breeds.length === 0) {
+        return res
+          .status(404)
+          .send(`There are no breed in the database by with ${req.params.color} eggs.`);
+      }
+      return res.status(200).json(breeds);
+    })
+    .catch((err) => {
+      return res.status(500).send(`Error: ${err}`);
+    });
+};
+
+// Get all breeds of specified class
+exports.breed_get_breeds_by_class = function (req, res) {
+  Breed.find({ apaClass: req.params.class })
+    .then((breeds) => {
+      // No breeds found. Abort.
+      if (breeds.length === 0) {
+        return res
+          .status(404)
+          .send(`There are no breeds of class ${req.params.class} in the database.`);
+      }
+      return res.status(200).json(breeds);
+    })
+    .catch((err) => {
+      return res.status(500).send(`Error: ${err}`);
+    });
+};
--- routes/breeds.js
+++ routes/breeds.js
@@ -1,72 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
+//Authentication
 const passport = require('passport');
 require('../passport');
-
-const { Breed } = require('../models/Breed.js');
-
 let auth = passport.authenticate('jwt', { session: false });
 
+//Controller
+const breed_controller = require('../controllers/breedController');
+
 // Get all chicken breeds
-router.get('/', auth, (req, res) => {
-  Breed.find()
-    .then((breeds) => {
-      return res.status(200).json(breeds);
-    })
-    .catch((err) => {
-      return res.status(500).send(`Something went wrong. Error: ${err}.`);
-    });
-});
+router.get('/', auth, breed_controller.breed_get_all_breeds);
 
 // Get one breed by name
-router.get('/:breed', auth, (req, res) => {
-  Breed.findOne({ breed: req.params.breed })
-    .then((breed) => {
-      // No breed found. Abort
-      if (!breed) {
-        return res.status(404).send('There are no breed in the database by that name.');
-      }
-      return res.status(200).json(breed);
-    })
-    .catch((err) => {
-      return res.status(500).send(`Error: ${err}`);
-    });
-});
+router.get('/:breed', auth, breed_controller.breeds_get_breed_by_name);
 
 // Get all breeds by egg color
-router.get('/eggs/:color', auth, (req, res) => {
-  Breed.find({ eggColor: req.params.color.toLowerCase() })
-    .then((breeds) => {
-      // No Breeds Found. Abort
-      if (breeds.length === 0) {
-        return res
-          .status(404)
-          .send(`There are no breed in the database by with ${req.params.color} eggs.`);
-      }
-      return res.status(200).json(breeds);
-    })
-    .catch((err) => {
-      return res.status(500).send(`Error: ${err}`);
-    });
-});
+router.get('/eggs/:color', auth, breed_controller.breed_get_breeds_by_egg_color);
 
 // Get all breeds of specified class
-router.get('/class/:class', auth, (req, res) => {
-  Breed.find({ apaClass: req.params.class })
-    .then((breeds) => {
-      // No breeds found. Abort.
-      if (breeds.length === 0) {
-        return res
-          .status(404)
-          .send(`There are no breeds of class ${req.params.class} in the database.`);
-      }
-      return res.status(200).json(breeds);
-    })
-    .catch((err) => {
-      return res.status(500).send(`Error: ${err}`);
-    });
-});
+router.get('/class/:class', auth, breed_controller.breed_get_breeds_by_class);
 
 module.exports = router;
