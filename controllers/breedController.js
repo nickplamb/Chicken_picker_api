208097494faba1ea229894ae00090c21d3577384
Nicholas Lamb <nickplamb@gmail.com>
2021-04-10 08:12:08 -0400

    fix query for get breed by name controller.

--- controllers/breedController.js
+++ controllers/breedController.js
@@ -14,7 +14,8 @@ exports.breed_get_all_breeds = function (req, res) {
 
 // Get one breed by name
 exports.breeds_get_breed_by_name = function (req, res) {
-  Breed.findByBreed(req.params.breed)
+  Breed.findOne()
+    .byBreed(req.params.breed)
     .then((breed) => {
       // No breed found. Abort
       if (!breed) {
