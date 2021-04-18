3589af4b9f8820ebaefdbc353faebb328e639b07
Nicholas Lamb <nickplamb@gmail.com>
2021-04-18 10:23:41 -0400

    add catch to db connection promise

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const host = process.env.CONNECTION_URI || 'mongodb://localhost:27017/chickendb';
 
 exports.connectDB = async () => {
-  const conn = await mongoose.connect(host, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const conn = await mongoose
+    .connect(host, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => console.log(`Error while connecting to db: ${err}`));
   console.log(`MongoDB connected: ${conn.connection.host}`);
 };
